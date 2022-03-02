29c48e373ca7b3aa77d4557a2dfa61585dc9ba9b
Kenneth C <kennethalonsoc@gmail.com>
2022-03-01 18:59:15 -0600

    Metodos Del Libro
    Se agregaron varios metodos del libro, aun no esta implementada la operacion de division

--- Main.py
+++ Main.py
@@ -11,16 +11,30 @@ class Menu(ttk.Frame):
         main_window.title("Calculadora FOC")
         self.main = main_window
         self.text = ttk.Label(self, text="Menu")
-        self.text.pack()
+        self.text.grid(row=0,column=1)
 
         self.btn = ttk.Button(self,command= self.go_techo_numero, text="Techo->Numero")
-        self.btn.pack(side=tk.LEFT)
+        self.btn.grid(row=1,column=0)
 
         self.btn_1 = ttk.Button(self,command = self.go_numero_techo, text="Numero->Techo")
-        self.btn_1.pack(side=tk.LEFT)
+        self.btn_1.grid(row=1,column=1)
 
         self.btn_2 = ttk.Button(self,command = self.go_operaciones, text="Operaciones")
-        self.btn_2.pack(side=tk.LEFT)
+        self.btn_2.grid(row=1,column=2)
+
+        self.btn_6 = ttk.Button(self,command = self.go_conversion, text="Conversion")
+        self.btn_6.grid(row=2,column=1)
+
+        self.btn_3 = ttk.Button(self,command = self.go_metodo_resta, text="Metodo Resta")
+        self.btn_3.grid(row=3,column=0)
+
+        self.btn_4 = ttk.Button(self,command = self.go_metodo_suma, text="Metodo Suma")
+        self.btn_4.grid(row=3,column=1)
+
+        self.btn_5 = ttk.Button(self,command = self.go_numeros_magicos, text="Numeros Magicos")
+        self.btn_5.grid(row=3,column=2)
+
+        
 
         self.pack()
 
@@ -35,6 +49,75 @@ class Menu(ttk.Frame):
     def go_operaciones(self):
         self.pack_forget()
         Operaciones(self.main)
+
+    def go_metodo_resta(self):
+        self.pack_forget()
+        Metodo_Resta(self.main)
+    
+    def go_metodo_suma(self):
+        self.pack_forget()
+        Metodo_Suma(self.main)
+
+    def go_numeros_magicos(self):
+        self.pack_forget()
+        Numeros_Magicos(self.main)
+
+    def go_conversion(self):
+        self.pack_forget()
+        Conversiones(self.main)
+
+class Numeros_Magicos(ttk.Frame):
+    def __init__(self, main_window):
+        super().__init__(main_window)
+        main_window.title("Calculadora FOC - Metodo de la Resta")
+        self.main = main_window
+        self.text = ttk.Label(self, text="Numeros Magicos")
+        self.text.grid(row=0, column=1)
+
+        self.button = ttk.Button(self,command= self.go_main, text="Menu")
+        self.button.grid(row=1, column=1)
+
+        self.txt_baseI = ttk.Label(self, text="Digite la base Incial")
+        self.txt_baseI.grid(row=3, column=0)
+
+        self.entry_baseI = self.entry = ttk.Entry(self)
+        self.entry_baseI.grid(row=3, column=2)
+
+        self.txt_baseF = ttk.Label(self, text="Digite la base Final")
+        self.txt_baseF.grid(row=4, column=0)
+
+        self.entry_baseF = self.entry = ttk.Entry(self)
+        self.entry_baseF.grid(row=4, column=2)
+
+        self.txt_num = ttk.Label(self, text="Digite la cantidad de Numeros Magicos")
+        self.txt_num.grid(row=5, column=0)
+
+        self.entry_num = self.entry = ttk.Entry(self)
+        self.entry_num.grid(row=5, column=2)
+
+        self.btn_calc = ttk.Button(self,command= self.calc, text="Calcular")
+        self.btn_calc.grid(row=6, column=1)
+
+        self.t_result = tk.StringVar()
+        self.t_result.set("Resultado = ")
+        self.txt_result =  ttk.Label(self, textvariable=self.t_result)
+        self.txt_result.grid(row=7, column=1)
+
+        self.pack()
+
+    def go_main(self):
+        self.pack_forget()
+        Menu(self.main)
+
+    def calc(self):
+        baseI = int(self.entry_baseI.get())
+        baseF = int(self.entry_baseF.get())
+        nums = int(self.entry_num.get())
+        resultado = ""
+        for i in range(nums):
+            if(i != 0):
+                resultado += "NM " + str(i) + " " + str((baseI**i)-(baseF**i)) + "\n"
+        self.t_result.set(resultado)
 
 class Techo_Numero(ttk.Frame):
     def __init__(self, main_window):
@@ -113,6 +196,103 @@ class Numero_Techo(ttk.Frame):
     def calc(self):
         self.t_result.set(numero_techo(int(self.entry_cantidad.get())))
 
+class Metodo_Resta(ttk.Frame):
+    def __init__(self, main_window):
+        super().__init__(main_window)
+        main_window.title("Calculadora FOC - Metodo de la Resta")
+        self.main = main_window
+        self.text = ttk.Label(self, text="Metodo de la Resta")
+        self.text.grid(row=0, column=1)
+
+        self.button = ttk.Button(self,command= self.go_main, text="Menu")
+        self.button.grid(row=1, column=1)
+
+        self.txt_cantidad = ttk.Label(self, text="Digite el Numero")
+        self.txt_cantidad.grid(row=3, column=0)
+
+        self.entry_cantidad = self.entry = ttk.Entry(self)
+        self.entry_cantidad.grid(row=3, column=2)
+
+        self.btn_calc = ttk.Button(self,command= self.calc, text="Calcular")
+        self.btn_calc.grid(row=4, column=1)
+
+        self.t_result = tk.StringVar()
+        self.t_result.set("Resultado = ")
+        self.txt_result =  ttk.Label(self, textvariable=self.t_result)
+        self.txt_result.grid(row=5, column=1)
+
+        self.pack()
+
+    def go_main(self):
+        self.pack_forget()
+        Menu(self.main)
+
+    def calc(self):
+        numero = int(self.entry_cantidad.get())
+        resultado = ""
+        num = ""
+        potencia = 0
+        while 2**potencia < numero:
+            potencia+=1
+        potencia-=1
+        while numero>0:
+            if(numero>=2**potencia):
+                resultado += "Potencia: " + str(potencia) + " Agregamos 1 y a " + str(numero) + " restamos " + str(2**potencia) + "\n"
+                numero-=(2**potencia)
+                num += " 1"
+                potencia-=1
+            else:
+                resultado += "Potencia: " + str(potencia) + " Agregamos 0 porque " + str(numero) + " no es mayor que " + str(2**potencia) + "\n"
+                num+=" 0"
+                potencia-=1
+        resultado += str(num)
+        self.t_result.set(resultado)
+
+class Metodo_Suma(ttk.Frame):
+    def __init__(self, main_window):
+        super().__init__(main_window)
+        main_window.title("Calculadora FOC - Metodo de la Resta")
+        self.main = main_window
+        self.text = ttk.Label(self, text="Metodo de la Suma")
+        self.text.grid(row=0, column=1)
+
+        self.button = ttk.Button(self,command= self.go_main, text="Menu")
+        self.button.grid(row=1, column=1)
+
+        self.txt_cantidad = ttk.Label(self, text="Digite el Numero")
+        self.txt_cantidad.grid(row=3, column=0)
+
+        self.entry_cantidad = self.entry = ttk.Entry(self)
+        self.entry_cantidad.grid(row=3, column=2)
+
+        self.btn_calc = ttk.Button(self,command= self.calc, text="Calcular")
+        self.btn_calc.grid(row=4, column=1)
+
+        self.t_result = tk.StringVar()
+        self.t_result.set("Resultado = ")
+        self.txt_result =  ttk.Label(self, textvariable=self.t_result)
+        self.txt_result.grid(row=5, column=1)
+
+        self.pack()
+
+    def go_main(self):
+        self.pack_forget()
+        Menu(self.main)
+
+    def calc(self):
+        numero = int(self.entry_cantidad.get())
+        num = str(numero)
+        resultado = ""
+        int_result = 0
+        for i in range(len(num)):
+            if num[len(num)-1-i] == "1":
+                int_result += 2**i
+                resultado += "Como el digito es 1 se eleva a " + str(i) + " sumando " + str(2**i) + "\n"
+            else:
+                resultado += "Como la posicion " + str(i)+ " es cero se continua " + "\n"
+        resultado += str(int_result)
+        self.t_result.set(resultado)
+        
 class Operaciones(ttk.Frame):
     def __init__(self, main_window):
         super().__init__(main_window)
@@ -203,6 +383,55 @@ class Operaciones(ttk.Frame):
             list_2.append(int(i))
 
         self.t_result.set(str(division_numeros(list_1,list_2,int(self.entry_base.get()))))
+
+class Conversiones(ttk.Frame):
+    def __init__(self, main_window):
+        super().__init__(main_window)
+        main_window.title("Calculadora FOC - Conversiones")
+        self.main = main_window
+        self.text = ttk.Label(self, text="Conversiones")
+        self.text.grid(row=0, column=1)
+
+        self.button = ttk.Button(self,command= self.go_main, text="Menu")
+        self.button.grid(row=1, column=1)
+ 
+        self.txt_cantidad = ttk.Label(self, text="Digite el Numero y la baseI")
+        self.txt_cantidad.grid(row=2, column=0)
+
+        self.entry_cantidad = ttk.Entry(self)
+        self.entry_cantidad.grid(row=2, column=1)
+
+        self.entry_baseI = ttk.Entry(self)
+        self.entry_baseI.grid(row=2, column=2)
+
+        self.txt_BaseF = ttk.Label(self, text="Digite la Base Final")
+        self.txt_BaseF.grid(row=3, column=0)
+
+        self.entry_baseF = ttk.Entry(self)
+        self.entry_baseF.grid(row=3, column=1)
+
+        self.btn_div = ttk.Button(self,command= self.convertir, text="Calcular")
+        self.btn_div.grid(row=4, column=1)
+
+        self.t_result = tk.StringVar()
+        self.t_result.set("Resultado = ")
+        self.txt_result =  ttk.Label(self, textvariable=self.t_result)
+        self.txt_result.grid(row=5, column=1)
+
+        self.pack()
+
+    def go_main(self):
+        self.pack_forget()
+        Menu(self.main)
+
+    def convertir(self):
+        list = []
+        for n in self.entry_cantidad.get().split("-"):
+            list.append(int(n))
+        decimal = base_decimal(list,int(self.entry_baseI.get()))
+        base = decimal_base(decimal, int(self.entry_baseF.get()))
+
+        self.t_result.set(str(base))
 
 main_window = tk.Tk()
 app = Menu(main_window)
